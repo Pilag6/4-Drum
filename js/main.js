9331d5a6f0e3941d4ddd45fd43c2480b974b3667
Pilag6 <pilag6@gmail.com>
2023-11-29 11:57:49 +0100

    Add audio files and update styles.css

---
+++ js/main.js
@@ -0,0 +1,61 @@
+function playSound(e) {
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
+    const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add("playing");
+}
+
+function removeTransition(e) {
+    if (e.propertyName !== "transform") return;
+
+    this.classList.remove("playing");
+}
+
+const keys = document.querySelectorAll(".key");
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playSound);
+
+// Play with mouse click
+
+const mouseKeys = document.querySelectorAll(".key");
+
+mouseKeys.forEach((key) => key.addEventListener("click", playSoundMouse));
+
+function playSoundMouse(e) {
+    const audio = document.querySelector(
+        `audio[data-key="${this.dataset.key}"]`
+    );
+    const key = document.querySelector(`.key[data-key="${this.dataset.key}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add("playing");
+}
+
+// Play with touch
+
+const touchKeys = document.querySelectorAll(".key");
+
+touchKeys.forEach((key) => key.addEventListener("touchstart", playSoundTouch));
+
+function playSoundTouch(e) {
+    const audio = document.querySelector(
+        `audio[data-key="${this.dataset.key}"]`
+    );
+    const key = document.querySelector(`.key[data-key="${this.dataset.key}"]`);
+
+    if (!audio) return;
+    audio.currentTime = 0;
+    audio.play();
+
+    key.classList.add("playing");
+}
+
+
